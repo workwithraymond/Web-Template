46e0f311ee10fd434bce852c586aa93c759d3448
workwithraymond <workwithraymond01@gmail.com>
2024-01-17 20:11:12 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
+
+const App = () => {
+
+
+
+  return (
+    <main>
+      <Navbar/>
+      <Hero/>
+      <Sidebar/>
+      <Submenu/>
+    </main>
+  )
+};
+export default App;
---
+++ src/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+      <h1>Manage Any Content <br /> Anywhere</h1>
+      <p>This company is the leading web development custom software.
+        It's 100% Javascript and fully customizable.
+      </p>
+      </div>
+    </div>
+  )
+}
+export default Hero
